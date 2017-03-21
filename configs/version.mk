62f0e552da40e4240d113ed8f479d0472df99311
viperusgit <viperusgit@gmail.com>
2017-03-20 20:58:19 -0400

    1.12 time ;)

--- configs/version.mk
+++ configs/version.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 VANILLA_BUILDNAME :=Ascent
-VANILLA_BUILDNUMBER :=1.11
+VANILLA_BUILDNUMBER :=1.12
 ifeq ($(PUBLIC),true)
  VANILLA_BUILDTAG :=Public
 else
